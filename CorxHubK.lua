f8ce468c2559226b81284192aed43a2547920d09
ApolloThdz <139158032+ApolloThdz@users.noreply.github.com>
2024-04-15 21:04:11 -0300

    CorxHubK.lua

---
+++ CorxHubK.lua
@@ -0,0 +1,87 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "CorxHub", HidePremium = false, SaveConfig = true, ConfigFolder = "CorxKey", IntroText = "Corx Hub Key System"})
+local Tab = Window:MakeTab({
+	Name = "Key System",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local player = game:GetService("Players").LocalPlayer
+OrionLib:MakeNotification({
+	Name = "Key System!",
+	Content = "Opa tudo bem, " .. player.Name .. ", Bem vindo ao Corx Hub",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+OrionLib:MakeNotification({
+	Name = "Key System!",
+	Content = "Por favor, " .. player.Name .. ", Insira a Chave de Ativação.",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+}
+
+_G.PActivateKey = "#Perm-AKmc3951sad9024"
+_G.PActivateInput = "string"
+_G.PActivateRKey = "null"
+
+function RandomK()
+    local s = ""
+    for i = 1, 15 do
+        if math.random() > 0.5 then
+            s = s .. string.char(math.random(65, 90))
+        else
+            s = s .. string.char(math.random(97, 122))
+        end
+    end
+    return s
+end
+
+function AutoKeyRandomizer()
+    local timer = 24 * 60 * 60 * 1000
+    while true do
+        wait(timer)
+        _G.PActivateRKey = RandomK()
+    end
+end
+  
+Tab:AddTextbox({
+    Name = "Chave de Ativação",
+    Default = "",
+    TextDisappear = true,
+    Callback = function(Value)
+    _G.PActivateInput = Value
+)
+
+Tab:AddButton({
+	Name = "Verify Key",
+	Callback = function()
+        if _G.PActivateInput == _G.PActivateKey then
+            print("Chave Correta. Ativando Script Hub")
+            OrionLib:MakeNotification({
+	          Name = "Key System!",
+	          Content = "Chave de Ativação Correta Executando o Script!!",
+	          Image = "rbxassetid://4483345998",
+	          Time = 5
+            })
+            loadstring(game:HttpGet(('')))()
+        else
+            OrionLib:MakeNotification({
+	          Name = "Key System!",
+	          Content = "Chave de Ativação Incorreta!",
+	          Image = "rbxassetid://4483345998",
+	          Time = 5
+            })
+            print("Chave Incorreta!")
+        end
+    end
+})
+
+local httpHook;
+httpHook = hookfunction(game.HttpGet, function(self,...)
+   local Args = {...}
+   if Args[1] and type(Args[1]) == "string" then
+       if Args[1]:match("checkPremium.php") then
+           return "true"
+       end
+   end
+   return httpHook(self,...)
+end)
