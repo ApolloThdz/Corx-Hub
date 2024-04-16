aaf62c3f41a6c0925a14f0676d3c50ae82442fc8
ApolloThdz <139158032+ApolloThdz@users.noreply.github.com>
2024-04-15 22:03:06 -0300

    Update CorxHub.lua

--- CorxHub.lua
+++ CorxHub.lua
@@ -1,6 +1,73 @@
-FSN = "First Sea"
-SSN = "Second Sea"
-TSN = "Third Sea"
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Corx Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Corx Hub", IntroText = "Corx Hub Loading...", IntroIcon = "rbxassetid://17165070203"})
+
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 First_Sea = false
 Second_Sea = false
 Third_Sea = false
@@ -12,3 +79,14 @@ elseif placeId == 4442272183 then
 elseif placeId == 7449423635 then
       Third_Sea = true
 end
+
+local httpHook;
+httpHook = hookfunction(game.HttpGet, function(self,...)
+   local Args = {...}
+   if Args[1] and type(Args[1]) == "string" then
+       if Args[1]:match("checkPremium.php") then
+           return "true"
+       end
+   end
+   return httpHook(self,...)
+end)
